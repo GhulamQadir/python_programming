cc121d47fdd51b334fcb9d6582b414576913f5d4
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2025-03-03 17:10:45 +0500

    python

---
+++ practise.py
@@ -0,0 +1,37 @@
+# userName = "Ali"
+# print(f'Hi {userName}, Would you like to learn some Python today?')
+
+
+# print(userName.upper())
+# print(userName.title())
+# print(userName.lower())
+
+
+# quote = "Eighty percent of success is showing up"
+# authorName = "Woody Allen"
+# print(f"{authorName} once said, \"{quote}.\"")
+
+
+# famous_person = "Woody Allen"
+# message = f"{famous_person} pnce said \"Eighty percent of success is showing up\""
+# print(message)
+
+
+# name ="\t\t\t\t\t  Hasan \t\t\n\n\n\t"
+# print(name)
+# print(name.lstrip())
+# print(name.rstrip())
+# print(name.strip())
+
+
+# split sentence and characters
+sentence = "We are learning Python"
+splitSentence = sentence.split()
+splitChars = list(sentence)
+print(splitSentence)
+print(splitChars)
+
+
+# counts the repeated alphabets in the given string
+fruit = "pomegranate"
+fruit.count("e")
