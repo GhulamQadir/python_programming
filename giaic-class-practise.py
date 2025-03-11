a90da1073468d36e7cee32fac181c3c80007c115
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2025-03-11 15:24:59 +0500

    updated

---
+++ giaic-class-practise.py
@@ -0,0 +1,26 @@
+#######################            DATA TYPES   ##############################3
+# # variable with type hinting is preferred(anytime we can assign different typed value in it nut not preferred)
+# charges: int = 30000
+# print(charges)
+# a = 70
+# a = "340"
+# print(a + charges)  # it will give error
+
+
+# # type casting(its hinting not type annotation)
+# val: int = 67.40076
+# print(type(val), "val_int: ", val) # it just hints that we should assign int type to it but not annotate
+
+
+# # complex number
+# numComplex: complex = 4 + 5j  # j is the sq.root of -1 (it's actually i)
+# ### real part is mandatory in imaginary like imaginary is never stand alone (j should be assign with any number)
+# print(numComplex)
+
+#complex wquation
+eq1:complex = (2+3j)*(1+2j)
+print(eq1)
+
+###################### data types ended ##################################
+
+
