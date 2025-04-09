34104628de2d76575b75df9280a61061311eb00c
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2025-04-09 22:18:11 +0500

    updated

--- giaic-class-practise.py
+++ giaic-class-practise.py
@@ -42,17 +42,85 @@
 #         Python"""
 # print(b)
 
+# LOOP
+for i in range(0, 10):
+    print(i)
 
-# for i in range(0, 10):
-#     print(i)
 
-
-# SET
+# SET  (only unique elements)
 mySet: set = {3, 4, 2, "2", 3}
-print(mySet)
+print(f"\n\n{mySet}")
 mySet.add(18)
 print(mySet)
 
 # frozen set (immutable)
 fSet: frozenset = frozenset([1, 2, 3, 433, 3, "Ali"])
 print(fSet)
+
+
+# TUPLE (unchangeable)
+myTuple: tuple = (3, 5, "Testing", [2, 3, 4, 5], False)
+print(f"\n\n{myTuple}")
+
+# we can create copy of array so that it does not affect its original array
+makeCopy = myTuple[3].copy()
+makeCopy[1] = 55
+print(makeCopy)
+print(myTuple)
+
+arr = myTuple[3]  # updating couple's array element by hack
+arr[1] = 55
+print(arr)
+print(myTuple)  # tuple updated because of memory reference
+
+
+# DICTIONARY
+myDict: dict = {
+    "name": "Saim",
+    "age": 23,
+    "rollNo": 2327,
+}
+print(f"\n\n{myDict}")
+print(myDict["age"])
+
+
+# TypeCasting
+i: int = 32
+print("\n\nValue of i = ", str(i))
+print("Type of i = ", str(type(i)))
+
+
+a: int = -10
+b: bool = bool(a)
+print("\n\nValue of b = ", str(b))  # "True"  value of string
+print("Type of b = ", str(type(b)))
+
+
+# check instance
+num: float = 77, 543.545
+print(f"\n\n{isinstance(num, int)}")
+
+
+myList: list[int] = [32, 43, 11, 2, 6]
+print("7 in my list: ", 7 in myList)
+print("10 is not in my list: ", 10 not in myList)
+
+
+# join method
+sentenceList: list = ["Python", "Programming"]
+joinSentence: str = " ".join(sentenceList)
+print(joinSentence)
+
+fruit: str = "Apple" * 3  # multiplying string
+print(fruit)
+
+
+# reference understanding
+std: object = {
+    "name": "Ali",
+    "age": 14,
+}  # this will be updated because its refernce is saved in std1
+std1 = std
+std1["name"] = "Shakir"  # both std and std1 will be updated
+print(std)
+print(std1)
